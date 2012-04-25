b52cf24ad0aa5d5e1a69f283fd788de1715244de
christian lenz <christian lenz@Pharma009.logiamed.local>
2012-04-25 15:36:24 +0200

    Code refactoring.

---
+++ Noise example/Scripts/test.js
@@ -0,0 +1,8 @@
+var Perlin = require("module/generator/perlin");
+console.log(Perlin);
+var perlin = new Perlin(); 
+
+// get a perlin noise value for the given coordinates  
+// var noiseValue = perlin.getValue( 0.25, 0.46, 0.12 );
+
+// console.log(noiseValue);
