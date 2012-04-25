fad723d4bcebba12bda5f9b857be4608cebcde8e
Enigmadt <new Version>
2012-04-25 13:56:44 +0200

    Änderungen

--- Noise example/js/game.js
+++ Noise example/js/game.js
@@ -15,11 +15,11 @@ renderer.setSize(window.innerWidth - 50, window.innerHeight - 50);
 
 controls = new THREE.FlyControls(camera);
 
-controls.movementSpeed = 2500;
+controls.movementSpeed = 500;
 
 controls.rollSpeed = Math.PI / 6;
 controls.autoForward = false;
-controls.dragToLook = false 
+controls.dragToLook = true; 
 
 /*
 controls = new THREE.TrackballControls(camera);
@@ -84,12 +84,12 @@ uniforms.texture3.texture.wrapS = uniforms.texture3.texture.wrapT = THREE.Repeat
 uniforms.texture4.texture.wrapS = uniforms.texture4.texture.wrapT = THREE.RepeatWrapping;
 uniforms.heightMap.texture.wrapS = uniforms.heightMap.texture.wrapT = THREE.RepeatWrapping;
 
-camera.position.z = 85100;
+camera.position.z = 801000;
 //controls.target = new THREE.Vector3(851000, 0, 0);
 
 var vShader = $("#vertexshader");
 var fShader = $("#fragmentshader");
-var scale=80000, detail=4,
+var scale=80000, detail=6,
 
     meshgeo = [[new THREE.IcosahedronGeometry(scale,detail),        scale*1.5],
                 [new THREE.IcosahedronGeometry(scale,detail-1),     scale*4],
@@ -267,7 +267,7 @@ function render() {
     THREE.SceneUtils.traverseHierarchy(scene, function (node) {if (node instanceof THREE.LOD) node.update(camera)});
     
     var delta = clock.getDelta();
-    controls.update(delta);
+    controls.update(delta/2);
 
     renderer.clear();
     renderer.render(scene, camera);
